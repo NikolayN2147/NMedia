f1e079a4309f520b2463014dcffb8a28e876afcb
NikolayN2147 <100046842+NikolayN2147@users.noreply.github.com>
2022-08-23 19:49:09 +0300

    add_hw_RecyclerView

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -8,8 +8,7 @@ class PostViewModel: ViewModel() {
 
     private val repository:PostRepository = InMemoryPostRepository()
 
-    val data = repository.get()
-    fun like() = repository.like()
-    fun repost() = repository.repost()
-
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun repostById(id: Long) = repository.repostById(id)
 }
