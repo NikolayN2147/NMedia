8f3aec581d9f80a4cb7dc655bcb3ecbf51a93f93
NikolayN2147 <100046842+NikolayN2147@users.noreply.github.com>
2022-09-18 23:29:45 +0300

    add_hw_Crud

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,14 +2,15 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.ru.netology.nmedia.viewmodel.PostViewModel
 
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,18 +18,58 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: PostViewModel by viewModels()
 
-        val adapter = PostsAdapter(
-            { viewModel.likeById(it.id) },
-            { viewModel.repostById(it.id) }
-        )
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            }
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+        })
+
         binding.list.adapter = adapter
-        viewModel.data.observe(this) { posts ->
+        viewModel.data.observe(this) { posts ->  //обновление данных
             adapter.submitList(posts)
+        }
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.content) {
+                requestFocus()         //как только edited меняется, обновляем текст
+                setText(post.content)  //как только edited меняется, обновляем текст
+            }
+        }
+
+        binding.save.setOnClickListener {
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
         }
 
 
     }
-
 }
 
 
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,14 +1,48 @@
 package ru.netology.nmedia.ru.netology.nmedia.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.Post
 import ru.netology.nmedia.ru.netology.nmedia.data.PostRepository
 import ru.netology.nmedia.ru.netology.nmedia.data.impl.InMemoryPostRepository
 
-class PostViewModel: ViewModel() {
+private val empty = Post(            //data-объект для заполнения
+    id = 0,
+    author = "",
+    content = "",
+    published = "",
+    likedByMe = false,
+    likes = 0,
+    reposts = 0
+)
 
-    private val repository:PostRepository = InMemoryPostRepository()
-
+class PostViewModel : ViewModel() {
+    // упрощённый вариант
+    private val repository: PostRepository = InMemoryPostRepository()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
+    fun save() {                          //функция сохранения
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {                //функция сохранения контента
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
     fun likeById(id: Long) = repository.likeById(id)
-    fun repostById(id: Long) = repository.repostById(id)
+    fun shareById(id: Long) = repository.shareById(id)
+    fun removeById(id: Long) = repository.removeById(id)
+//    fun share() = repository.share()
 }
